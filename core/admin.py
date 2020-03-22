03f0a888e5c70f64b43365727e42e0dacdd09274
blastz13 <sinicinr2003@mail.ru>
2020-03-22 21:31:12 +0300

    Add model Feed and output on the main page

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Feed
+
+
+@admin.register(Feed)
+class AdminFeed(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'body', 'get_image', 'is_slider', 'is_blog', 'is_publish', 'date_publicate']
+    list_display_links = ['title', 'slug', 'body', 'get_image']
+
+    def get_image(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src={obj.image.url} width="640" height="360"')
+
+    get_image.short_description = "Фото"
--- core/views.py
+++ core/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from django.utils import timezone
+from datetime import datetime
+
+from .models import Feed
+
 
 def index(request):
-    return render(request, 'core/index.html')
+    slider_feeds = Feed.objects.filter(is_publish=True,
+                                       is_slider=True,
+                                       date_published_from__lte=datetime.now(tz=timezone.utc),
+                                       date_published_to__gte=datetime.now(tz=timezone.utc)
+                                       )
+
+    small_feeds = Feed.objects.filter(is_publish=True,
+                                      is_blog=True,
+                                      date_published_from__lte=datetime.now(tz=timezone.utc),
+                                      date_published_to__gte=datetime.now(tz=timezone.utc)
+                                      )
+
+    return render(request, 'core/index.html', context={"feeds": slider_feeds,
+                                                       "small_feeds": small_feeds,
+                                                       })
 # Create your views here.
