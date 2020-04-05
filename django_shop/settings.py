f01279a449c0bac9ddeb968db4200448f492d85b
blastz13 <sinicinr2003@mail.ru>
2020-04-05 16:43:22 +0300

    install postgresql db

--- django_shop/settings.py
+++ django_shop/settings.py
@@ -78,10 +78,14 @@ WSGI_APPLICATION = 'django_shop.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+     'default': {
+     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+     'NAME': 'django_db',
+     'USER': 'postgres',
+     'PASSWORD': '53wgn9te',
+     'HOST': 'localhost',
+     'PORT': '5432',
+ }
 }
 
 # Password validation
