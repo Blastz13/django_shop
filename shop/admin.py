67293092e5754f0180a692cc19d1ca6c6d96fe3b
blastz13 <sinicinr2003@mail.ru>
2020-04-06 19:29:41 +0300

    Added model Category for Product

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+
 from .models import Product
+from .models import Category
 
 from django.contrib.postgres.fields import JSONField
 from django_json_widget.widgets import JSONEditorWidget
@@ -7,9 +10,17 @@ from django_json_widget.widgets import JSONEditorWidget
 
 @admin.register(Product)
 class AdminProduct(admin.ModelAdmin):
-    list_display = ['title', 'price', 'discount_price', 'slug']
-    list_display_links = ['title', 'price', 'discount_price', 'slug']
-    search_fields = ('title', 'description', 'price', 'discount_price')
+    list_display = ['title', 'category', 'price', 'discount_price', 'slug']
+    list_display_links = ['title', 'category', 'price', 'discount_price', 'slug']
+    search_fields = ('title', 'category', 'description', 'price', 'discount_price')
     formfield_overrides = {
         JSONField: {'widget': JSONEditorWidget},
     }
+
+
+@admin.register(Category)
+class AdminCategory(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'parent']
+    list_display_links = ['title', 'slug', 'parent']
+    search_fields = ['title', 'slug', 'parent']
+
--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
+from mptt.models import MPTTModel, TreeForeignKey
 
 
 class Product(models.Model):
@@ -8,6 +9,7 @@ class Product(models.Model):
     slug = models.SlugField(unique=True, verbose_name='Ссылка на товар')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена товара')
     discount_price = models.PositiveIntegerField(blank=True, verbose_name='Цена по скидке')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='product', verbose_name='Категория')
     is_available = models.BooleanField(verbose_name='Товар в наличии')
     property = JSONField(blank=True, null=True, verbose_name='Свойства товара')
     date_publicate = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления товара')
@@ -21,3 +23,22 @@ class Product(models.Model):
         ordering = ['order', '-date_publicate']
         verbose_name = "Товар"
         verbose_name_plural = "Товары"
+
+
+class Category(MPTTModel):
+    title = models.CharField(max_length=64, unique=True, verbose_name='Название категории')
+    slug = models.SlugField(max_length=64, unique=True, verbose_name='Ссылка категории')
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='child', verbose_name='Подкатегория')
+
+    def get_category_url(self):
+        return '/'.join([x['slug'] for x in self.get_ancestors(include_self=True).values()])
+
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    class MPTTMeta:
+        order_insertion_by = ['title']
