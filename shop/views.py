6867e6c0d857d683240f5900b66679e4290cd2f3
Blastz13 <blastz13@mail.ru>
2021-07-05 11:02:44 +0300

    Сhanged the logic of the new store slider

--- shop/views.py
+++ shop/views.py
@@ -26,7 +26,7 @@ class ProductList(ObjectSortPaginate, View):
                                               price__lte=price_range_max_filter)
         context = self.get_pagination(all_products, 12)
         context['all_category'] = Category.objects.all()
-        context['price_range'] = all_products.aggregate(Min('price'), Max('price'))
+        context['price_range'] = Product.objects.filter(is_publish=True).aggregate(Min('price'), Max('price'))
         context['price_range']['price__min'] = str(context['price_range']['price__min'])
         context['price_range']['price__max'] = str(context['price_range']['price__max'])
         return render(request, 'shop/shop.html', context=context)
@@ -74,7 +74,8 @@ class CategoryProduct(ObjectSortPaginate, View):
                                                           price__lte=price_range_max_filter)
             context = self.get_pagination(products_by_category)
             context['all_category'] = Category.objects.all()
-            context['price_range'] = products_by_category.aggregate(Min('price'), Max('price'))
+            context['price_range'] = Product.objects.filter(category__in=category.get_descendants(include_self=True),
+                                                            is_publish=True).aggregate(Min('price'), Max('price'))
             context['price_range']['price__min'] = str(context['price_range']['price__min'])
             context['price_range']['price__max'] = str(context['price_range']['price__max'])
             context['obj_selected_category'] = category
