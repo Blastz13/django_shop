a8e418b7a6a5694429d2c1958b65f6cc25e92778
Blastz13 <sinicinr2003@mail.ru>
2021-07-10 19:26:02 +0300

    Added widget top related products

--- shop/templatetags/filters_shop.py
+++ shop/templatetags/filters_shop.py
@@ -1,6 +1,7 @@
 from django import template
 
 from core.models import OurBrand
+from shop.models import Product
 
 from shop.cart import Cart
 
@@ -30,3 +31,13 @@ def breadcrumb_area_shop(obj_selected_category=None):
 def widget_cart(request):
     cart = Cart(request)
     return {'cart': cart}
+
+
+@register.inclusion_tag('shop/inclusion_html/widget-top-rated-products.html')
+def widget_top_rated_products(obj_selected_category=None):
+    if obj_selected_category:
+        top_rated_products = Product.objects.filter(category=obj_selected_category,
+                                                    is_publish=True).order_by('-count_views')[:5]
+    else:
+        top_rated_products = Product.objects.filter(is_publish=True).order_by('-count_views')[:5]
+    return {'top_rated_products': top_rated_products}
